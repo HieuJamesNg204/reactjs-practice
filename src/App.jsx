a2cade33a8f1e02430c066783cd3e13f4423ec3b
Hieu Nguyen <HieuJames204@gmail.com>
2024-12-10 14:33:17 +0700

    React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <ol>
+        <li>React is a popular library</li>
+        <li>I'm more likely to get a job as a full stack dev</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <div>
-    <img src="/src/assets/react.svg" />
-    <h1>This is another element</h1>
-  </div>
+  <App />
 );
